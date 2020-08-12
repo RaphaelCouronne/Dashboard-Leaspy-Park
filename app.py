248532d71e53f6334d2d9141ed523642d0b17c1b
Raphael <raphael.couronne@gmail.com>
2020-08-12 15:55:25 +0200

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ server = app.server
 
 # Define title and layout
 app.title = 'COVID 19 - World cases'
-app.layout = html.H1("COVID 19 🦠 - Day to day evolution all over the world", className="header__text")
+app.layout = html.H1("COVID 19 🦠 - Day to day evolution all over the world v2", className="header__text")
 
 if __name__ == "__main__":
     # Starting flask server
